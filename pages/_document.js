035241690eb889928a940bf12ddf4338915ed3eb
Ahmed Yasser <ayasser@kwentra.com>
2023-04-14 20:38:09 +0200

    edit as in link

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@4cac1a6/css/all.css" rel="preload" as="style" onLoad='this.onload=null;this.rel="stylesheet"' type="text/css" />
+      <link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@4cac1a6/css/all.css" rel="preload" as="stylesheet" onLoad='this.onload=null;this.rel="stylesheet"' type="text/css" />
       </Head>
       <body dir='rtl'>
         <Main />
