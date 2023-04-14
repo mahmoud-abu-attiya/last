ac4c9456a2dc1531aa9700f02f2c4637b45192cf
Ahmed Yasser <ayasser@kwentra.com>
2023-04-14 20:23:55 +0200

    font awsome in last doc

--- pages/_document.js
+++ pages/_document.js
@@ -3,13 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-      <link defer href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@4cac1a6/css/all.css" rel="stylesheet" type="text/css" />
-      </Head>
       <body dir='rtl'>
         <Main />
         <NextScript />
       </body>
+      <link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@4cac1a6/css/all.css" rel="stylesheet" type="text/css" />
     </Html>
   )
 }
