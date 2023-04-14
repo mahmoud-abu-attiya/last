656ec84fdc2f830bb40e2c369f86c7dd81cfc3ba
Ahmed Yasser <ayasser@kwentra.com>
2023-04-14 20:35:04 +0200

    defer font awesome

--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <Head>
+      <link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@4cac1a6/css/all.css" rel="preload" as="style" onLoad='this.onload=null;this.rel="stylesheet"' type="text/css" />
+      </Head>
       <body dir='rtl'>
         <Main />
         <NextScript />
       </body>
-      <link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@4cac1a6/css/all.css" rel="stylesheet" type="text/css" />
     </Html>
   )
 }
