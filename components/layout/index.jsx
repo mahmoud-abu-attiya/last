11b39efdd707a7d86d6a5de5e6f37b275dd78597
Ahmed Yasser <ayasser@kwentra.com>
2023-04-14 21:03:39 +0200

    create font awsome link in layout

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Header from './header/Header'
 import Footer from './footer/Footer'
 import { useDispatch } from 'react-redux'
@@ -7,6 +7,14 @@ import { setSettingsData } from '../../slices/settingsSlices'
 export default function Layout({ children, settings, countries, footerCountries }) {
    const dispatch = useDispatch()
    dispatch(setSettingsData(settings))
+   useEffect(() => {
+      if (document.fontadded) return
+      document.fontadded = true
+      const link = document.createElement('link')
+      link.rel = 'stylesheet'
+      link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'
+      document.head.appendChild(link)
+   })
    return (
       <>
          <Header countries={countries} />
--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,6 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-      <link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@4cac1a6/css/all.css" rel="preload" as="stylesheet" onLoad='this.onload=null;this.rel="stylesheet"' type="text/css" />
-      </Head>
       <body dir='rtl'>
         <Main />
         <NextScript />
