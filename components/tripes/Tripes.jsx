6a5623f75c247557fe9776e51fec3c725f7b6fdf
Ahmed Yasser <ayasser@kwentra.com>
2023-04-14 19:42:59 +0200

    tripes

--- components/tripes/Tripes.jsx
+++ components/tripes/Tripes.jsx
@@ -1,89 +1,148 @@
 import styles from './index.module.css'
-import { useState, useRef } from 'react'
+import { useState, useMemo } from 'react'
 import TripesCard from './tripesCard'
 import Link from 'next/link'
 import Image from 'next/image'
-import { useSelector } from 'react-redux'
 
 const Tripes = ({ data: countries }) => {
-  const swp = useSelector((state) => state.renderSwiper.value)
   const [value, setValue] = useState(0)
-  const filteredCountries =
-    value === 0
-      ? countries
-      : value === 1
-      ? countries.filter((country) => country?.type === 'out')
-      : countries.filter((country) => country?.type === 'in')
+  const filteredCountries = useMemo(() => {
+    if (value === 0) {
+      return countries;
+    } else if (value === 1) {
+      return countries.filter((country) => country?.type === 'out');
+    } else {
+      return countries.filter((country) => country?.type === 'in');
+    }
+  }, [value, countries]);
+
+  const slides = filteredCountries.concat(filteredCountries.slice(0, 4))
+  slides.unshift(filteredCountries[filteredCountries.length - 1])
+  const step = 100 / slides.length
+  let [translate, setTranslate] = useState(step)
+  let [index, setIndex] = useState(1)
+  const [transition, setTransition] = useState(true)
+  const [canClick, setCanClick] = useState(true)
+
+  const next = () => {
+    if (!canClick) return
+    setCanClick(false)
+    setIndex(index + 1)
+    setTranslate((index + 1) * (step))
+    if (index === slides.length - 5) {
+      setTimeout(() => {
+        setTransition(false)
+        setTranslate(step)
+        setIndex(1)
+        setTimeout(() => {
+          setTransition(true)
+          setCanClick(true)
+        }, 10)
+      }, 500);
+    } else {
+      setTimeout(() => {
+        setCanClick(true)
+      }, 500);
+    }
+  }
+
+  const prev = () => {
+    if (!canClick) return
+    setCanClick(false)
+    if (index === 1) {
+      setTransition(false)
+      setTranslate((100 / slides.length) * (slides.length - 4))
+      setIndex(slides.length - 4)
+      translate = (100 / slides.length) * (slides.length - 4)
+      index = slides.length - 4
+      setTimeout(() => {
+        setTransition(true)
+        setIndex(index - 1)
+        setTranslate(translate - 100 / slides.length)
+        setTimeout(() => {
+          setCanClick(true)
+        }, 500);
+      }, 10);
+    } else {
+      setIndex(index - 1)
+      setTranslate(translate - 100 / slides.length)
+      setTimeout(() => {
+        setCanClick(true)
+      }, 500);
+    }
+
+  }
 
   const tripesTabs = ['جميع الوجهات', 'الوجهات الخارجية', 'الوجهات الداخلية']
   return (
-    <div className={styles.tripes}>
+    <div className={`grid grid-cols-6 ${styles.tripes}`}>
+      <div className="col-span-6 lg:col-span-1 pt-16 lg:py-20 lg:border-l flex flex-col justify-between items-center">
+        <Link href='/our-programs' className={"border-b pb-2 w-fit hidden lg:block"}>
+        عرض الكل
+      </Link>
       <h2
-        className={styles.tripes__title}
+        className={`lg:hidden ${styles.tripes__title}`}
       >
         اختر وجهتك الان
       </h2>
-      <div className={styles.tripes__container}>
         <div
-          className={styles.tripes__tabs}
+          className={` flex-row lg:flex-col justify-center ${styles.tripes__tabs}`}
         >
           {tripesTabs.map((tab, i) => (
             <button
               key={i}
               onClick={() => setValue(i)}
-              className={`${styles.tripes__tab} ${
-                value === i ? styles.tripes__tab__active : ''
-              }`}
+              className={`${styles.tripes__tab} border-secondary border-none px-4 ${value == i ? "md:border-b-2 text-secondary" : "text-gray-600"}`}
             >
               {tab}
             </button>
           ))}
+          <span className={`absolute transition-all hidden lg:flex left-0 -translate-x-1/2 justify-center items-center -translate-y-1/2 w-16 h-8 bg-white ${value == 0 ? "top-[10%]" : value == 1 ? "top-1/2" : "top-[90%]"}`}>
+            <span className="bg-secondary h-1 w-full block"></span>
+          </span>
         </div>
-          {/* <Swiper
-            slidesPerView={'auto'}
-            centeredSlides={true}
-            spaceBetween={20}
-            breakpoints={{
-              768: {
-                slidesPerView: 2,
-                centeredSlides: true,
-                spaceBetween: 10,
-              },
-              1024: {
-                slidesPerView: 3,
-              },
-            }}
-            loop={true}
-            grabCursor={true}
-            navigation={{
-              prevEl: swiperPrevRef.current,
-              nextEl: swiperNextRef.current,
-            }}
-            modules={[Navigation]}
-            className={styles.tripes__content}
-            onInit={(swiper) => {
-              swiper.params.navigation.prevEl = swiperPrevRef.current
-              swiper.params.navigation.nextEl = swiperNextRef.current
-              swiper.navigation.init()
-              swiper.navigation.update()
-            }}
-          >
-            {filteredCountries?.map((country) => (
-              <SwiperSlide key={country.id} className={styles.tripes__slide}>
-                <TripesCard item={country} />
-              </SwiperSlide>
-            ))}
-            <div className={styles.swiper__prev} ref={swiperPrevRef}>
-              <Image src='/prev-arrow-gray.svg' alt='' width={65} height={65} />
-            </div>
-            <div className={styles.swiper__next} ref={swiperNextRef}>
-              <Image src='/next-arrow-gray.svg' alt='' width={65} height={65} />
-            </div>
-          </Swiper> */}
+      <div className='hidden lg:block'>
+        <button className="btn p-4 group" onClick={prev}>
+          <Image src="/icons/prev-arrow.svg" className='group-hover:translate-x-4 transition' alt="arrow-left" width={60} height={60} />
+        </button>
+        <button className="btn p-4 group" onClick={next}>
+          <Image src="/icons/next-arrow.svg" className='group-hover:-translate-x-4 transition duration-300' alt="arrow-right" width={60} height={60} />
+        </button>
       </div>
-      <Link href='/our-programs' className={styles.view__all}>
+      </div>
+      <div className={` col-span-6 lg:col-span-5 lg:pr-10 lg:py-20`}>
+      <h2
+        className={`hidden lg:block ${styles.tripes__title}`}
+      >
+        اختر وجهتك الان
+      </h2>
+        <div className='w-full overflow-hidden'>
+          <div className='swiper-wrapper'>
+            <div className={`w-fit flex gap-4 ${transition && "transition duration-500"}`} style={{ transform: `translate(${translate}%)` }}>
+              {slides.map((slide, index) => {
+                return (
+                  <div key={index}>
+                    <TripesCard
+                      item={slide}
+                    />
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </div>
+        <div className='block lg:hidden text-center'>
+        <button className="btn p-4 group" onClick={prev}>
+          <Image src="/icons/prev-arrow.svg" className='group-hover:translate-x-4 transition' alt="arrow-left" width={60} height={60} />
+        </button>
+        <button className="btn p-4 group" onClick={next}>
+          <Image src="/icons/next-arrow.svg" className='group-hover:-translate-x-4 transition duration-300' alt="arrow-right" width={60} height={60} />
+        </button>
+      </div>
+      <Link href='/our-programs' className="border-b-2 text-secondary text-xs w-fit mx-auto block pb-1 lg:hidden">
         عرض الكل
       </Link>
+      </div>
     </div>
   )
 }
--- components/tripes/tripesCard.jsx
+++ components/tripes/tripesCard.jsx
@@ -1,29 +1,27 @@
 import styles from './index.module.css'
 // import { BsArrowUpLeft } from 'react-icons/bs'
-import AnimatedBtn from '../animatedBtn'
+import BtnArrow from '../BtnArrow'
 import Image from 'next/image'
 import { useSelector } from 'react-redux'
 
 const TripesCard = ({ item }) => {
   const settings = useSelector((state) => state.settings.value)
   return (
-    <div className={styles.card}>
-      <div className={styles.card__container}>
-        <div className={styles.card__front}>
-          <Image
-            src={item.image}
-            alt={item.name}
-            fill
-            quality={50}
-            className={styles.front__img}
-          />
-          <div className={styles.card__front__content}>
-            <h3 className={styles.front__title}>{item.name}</h3>
-            {/* <BsArrowUpLeft className={styles.front__icon} /> */}
-          </div>
+    <div className="flip-card rounded-3xl my-14 relative">
+    <div className="flip-card-inner rounded-3xl">
+      <div className="flip-card-front rounded-3xl overflow-hidden p-4 flex">
+        <Image src={item.image} alt={item.name} fill className="object-cover" />
+        <div className="flex w-full gap-8 justify-between items-center mt-auto">
+          <h3 className="title md:text-xl lg:text-2xl text-white font-bold mb-4">
+            {item.name}
+          </h3>
+          <span className="text-primary min-w-[3rem] w-12 h-12 border border-primary rounded-full flex justify-center items-center">
+            <i className="fal fa-arrow-left rotate-45"></i>
+          </span>
         </div>
-        <div className={styles.card__back}>
-          <Image
+      </div>
+      <div className={`flip-card-back rounded-3xl overflow-hidden bg-white text-secondary p-4 sm:p-6 flex flex-col gap-2 sm:gap-5 items-start ${styles.card__back}`}>
+      <Image
             src={settings.logo}
             alt={`tripes logo ${item.name}`}
             width={102}
@@ -32,17 +30,16 @@ const TripesCard = ({ item }) => {
           />
           <h3 className={styles.back__title}>{item.name}</h3>
           <div className={styles.card__btn}>
-            <AnimatedBtn
-              text='تفاصيل البرنامج'
-              textColor='#07162d'
-              url={`/${
+            <BtnArrow
+              title='تفاصيل البرنامج'
+              href={`/${
                 item.country_for === 'discounts' ? 'sales' : 'our-programs'
               }/${item.id}`}
             />
           </div>
-        </div>
       </div>
     </div>
+  </div>
   )
 }
 
