cc48c5ec11736b5487ff8c2da410ce353e4f5ddc
Mahmoud Abu-Attiya <mahmoud.abuattiya106@gmail.com>
2023-04-25 21:20:32 +0200

    new theme for our programs page

--- components/Breadcrumbs.jsx
+++ components/Breadcrumbs.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Breadcrumbs = ({ list }) => {
    return (
-      <nav className="flex border-b py-4" aria-label="Breadcrumb">
+      <nav className="flex py-4" aria-label="Breadcrumb">
          <ol className="inline-flex items-center gap-4">
             {list.map((item, i) => <li key={i} className="inline-flex items-center">
                <Link href={item.href || "#"} className="inline-flex items-center text-xs md:text-sm gap-4 font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
@@ -13,8 +13,6 @@ const Breadcrumbs = ({ list }) => {
             </li>)}
          </ol>
       </nav>
-
-
    )
 }
 
--- pages/our-programs/index.jsx
+++ pages/our-programs/index.jsx
@@ -4,9 +4,13 @@ import Image from 'next/image'
 import Head from 'next/head'
 import FullPageSlider from '../../components/fullPageSlider'
 import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
 
 const OurPrograms = ({ countries, slides }) => {
   const settings = useSelector(state => state.settings.value);
+  useEffect(() => {
+    console.log(countries);
+  }, [])
   return (
     <>
       <Head>
@@ -38,21 +42,34 @@ const OurPrograms = ({ countries, slides }) => {
         btnText={slides[0]?.button_text}
         btnUrl={'#content'}
       />
-      <div className={styles.programs__content} id='content'>
+      <div className={`bg-gray-50 ${styles.programs__content}`} id='content'>
         <h2 className='main__title'>البرامج السياحية</h2>
-        <div className={styles.programs__grid}>
+        <p className='max-w-2xl text-center mb-8'> غينيا واستمر العصبة ضرب قد. وباءت الأمريكي الأوربيين هو به،, هو العالم، الثقيلة بال. مع وايرلندا الأوروبيّون كان, قد بحق أسابيع العظمى واعتلاء. انه كل وإقامة المواد.  غينيا واستمر العصبة ضرب قد. وباءت الأمريكي الأوربيين هو به،, هو العالم، الثقيلة بال. مع وايرلندا الأوروبيّون كان, قد بحق أسابيع العظمى واعتلاء. انه كل وإقامة المواد.</p>
+        <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8 container`}>
           {countries.map((country) => ( // remove reverse() to egnore the error
               <Link 
               href="/our-programs/[place]"
               as={`/our-programs/${country.id}`} 
-              key={country.id} className={styles.programs__card}>
+              key={country.id} className={`h-[20rem] group relative rounded-xl overflow-hidden`}>
                   <Image
                     fill
                     src={country.image}
                     alt={'country image'}
-                    className={styles.programs__card__img}
+                    className={`object-cover group-hover:scale-110 transition-all duration-500 ease-in-out`}
                   />
-                  <h3>{country.name}</h3>
+                  <div className="flex flex-col gap-4 justify-end h-full w-full p-4 bg-gradient-to-t from-black/75 relative z-10 text-white to-transparent">
+                    <h3 className='text-xl md:text-2xl'>{country.name}</h3>
+                    {/* {type == "out" ?
+                      (<i className="fas fa-plane-alt"></i> country.type
+                     : 
+                      (<i className="fas fa-taxi"></i> country.type)
+                    } */}
+                    {country.type == "out" ?
+                      <div className='flex gap-4 items-center'><i className="fas fa-plane-alt md:text-xl -rotate-45"></i>{country.type}</div>
+                     :  
+                      <div className='flex gap-4 items-center'><i className="fas fa-taxi md:text-xl"></i>{country.type}</div>
+                      }
+                  </div>
               </Link>
             ))}
         </div>
