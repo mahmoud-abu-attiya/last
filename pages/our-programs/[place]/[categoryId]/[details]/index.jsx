76ef419bfb05feed9168bae7b6b28a6c1e8e6471
Mahmoud Abu-Attiya <mahmoud.abuattiya106@gmail.com>
2023-05-02 22:52:57 +0300

    wrapimage

--- pages/our-programs/[place]/[categoryId]/[details]/index.jsx
+++ pages/our-programs/[place]/[categoryId]/[details]/index.jsx
@@ -228,7 +228,9 @@ const Details = (props) => {
       </Head>
       <div className='bg-gray-50'>
         <div className={styles.details__bg}>
+          <div className='w-full h-full absolute'>
           <Image src={program?.image} alt={program.title} fill unoptimized={true} />
+          </div>
           <div className={styles.content}>
             <h1>
               {program.category?.name} - {program.country?.name}
