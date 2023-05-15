28945a9c89f80789278eb02557b84eefdd1e17e0
Mahmoud Abu-Attiya <mahmoud.abuattiya106@gmail.com>
2023-05-15 21:18:37 +0300

    return gtm

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Favicon from "@/components/favicon";
-// import Script from "next/script";
+import Script from "next/script";
 
 export default function Document() {
    return (
@@ -37,7 +37,7 @@ export default function Document() {
             <meta property="fb:admins" content="USER_ID"/>
             <meta property="fb:app_id" content="APP_ID"/>
             */}
-            {/* <Script id="google-tag-manager" strategy="afterInteractive">
+            <Script id="google-tag-manager" strategy="afterInteractive">
                {`
                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -45,16 +45,16 @@ export default function Document() {
                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                })(window,document,'script','dataLayer','GTM-W4V2HT3');
                `}
-            </Script> */}
+            </Script>
          </Head>
          <body dir="rtl" className="dark:bg-gray-950">
             <Main />
             <NextScript />
-            {/* <noscript
+            <noscript
                dangerouslySetInnerHTML={{
                   __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W4V2HT3" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
                }}
-            /> */}
+            />
          </body>
       </Html>
    );
