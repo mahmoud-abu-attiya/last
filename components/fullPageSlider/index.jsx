8bf4931bb71afd5c1a04a760037cdfe60de5271e
Mahmoud Abu-Attiya <mahmoud.abuattiya106@gmail.com>
2023-05-22 17:32:43 +0300

    try

--- components/fullPageSlider/index.jsx
+++ components/fullPageSlider/index.jsx
@@ -25,7 +25,7 @@ const FullPageSlider = ({ title, data }) => {
       {data.map((item, i) => {
         return (
           <div className={`transition duration-500 ${i === activeIndex ? "opacity-100" : "opacity-0"}`} key={i}>
-            <Image src={item.image} alt="hero" fill className={`object-cover`} loading='eager' />
+            <Image src={item.image} alt="hero" fill className={`object-cover`} priority />
             <div className={styles.slider__content}>
                 <div className={styles.slider__container}>
                   <h1 className={styles.slider__title}>
--- pages/sea-tripes/index.jsx
+++ pages/sea-tripes/index.jsx
@@ -62,34 +62,32 @@ const SeaTripes = ({
       <div className={styles.seaTripes__content} id='content'>
         <h2 className='main__title'>الرحلات البحرية</h2>
         <div className={styles.offer__cards}>
-          {programs?.map((card) => (
+          {programs.map((card) => (
             <div className={styles.offer__card} key={card.id}>
               <div className={styles.offer__card__container}>
                 <div className={styles.offer__img__container}>
                   <Image
-                    src={card?.image}
-                    alt={card?.title}
+                    src={card.image}
+                    alt={card.title}
                     fill
                     className={styles.offer__card__img}
+                    quality={50}
                   />
                 </div>
                 <div className={` dark:bg-gray-900 dark:text-white ${styles.offer__card__content}`}>
                   <div className={styles.offer__card__period}>
                     <span>
-                      {/* <i className="fas fa-sun"></i> */}
                       <FontAwesomeIcon icon={faSun} />
-                      {card?.days} أيام
+                      {card.days} أيام
                     </span>
                     <span>
-                      {/* <i className="fas fa-moon"></i> */}
                       <FontAwesomeIcon icon={faMoon} />
-                      {card?.nights} ليالي
+                      {card.nights} ليالي
                     </span>
-                    {card?.people && (
+                    {card.people && (
                       <span>
-                        {/* <i className="fas fa-user-friends"></i> */}
                         <FontAwesomeIcon icon={faUserFriends} />
-                        {card?.people}
+                        {card.people}
                       </span>
                     )}
                   </div>
@@ -105,7 +103,6 @@ const SeaTripes = ({
                     </a>
                     <div className={styles.stars}>
                       {Array.from(Array(card.rate)).map((s, i) => (
-                        // <i className="fas fa-star text-yellow-400" key={i}></i>
                         <FontAwesomeIcon icon={faStar} key={i} className='text-yellow-400' />
                       ))}
                     </div>
@@ -113,7 +110,7 @@ const SeaTripes = ({
                   <div className={styles.offer__card__price}>
                     <div>
                       <span className={styles.new__price}>
-                        {card?.price_after_discount}
+                        {card.price_after_discount}
                       </span>{' '}
                       ريال سعودي
                     </div>
@@ -132,7 +129,7 @@ const SeaTripes = ({
                           marginRight: '.5rem',
                         }}
                       >
-                        {card?.price}
+                        {card.price}
                       </span>
                     </p>
                   </div>
@@ -141,14 +138,14 @@ const SeaTripes = ({
                       <path d='m30.718 13.042.002-9.522a2.243 2.243 0 0 0-2.24-2.24l-9.52.002-.402-.002c-.83 0-1.62.048-2.19.618L1.822 16.442a1.83 1.83 0 0 0-.542 1.308c0 .495.192.96.542 1.308l11.12 11.12c.348.35.813.542 1.308.542.492 0 .96-.192 1.308-.542l14.544-14.546c.626-.622.62-1.52.618-2.384l-.002-.206zM24.96 8.96a1.92 1.92 0 1 1 .001-3.841 1.92 1.92 0 0 1-.001 3.841z'></path>
                     </svg>
                     وفر:
-                    <span>{card?.price - card?.price_after_discount}</span>
+                    <span>{card.price - card.price_after_discount}</span>
                     ريال سعودي
                   </p>
                   <div className={styles.offer__card__btns}>
                     <BtnArrow
                       title='حجز العرض'
                       href={`https://api.whatsapp.com/send?phone=${settings.whatsup
-                        }&${message(card?.id)}`}
+                        }&${message(card.id)}`}
                     />
                   </div>
                 </div>
